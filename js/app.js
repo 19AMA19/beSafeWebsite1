3f8fbe1285615d6acc4e0e1bf5a644fadbac9991
Abdulkarim <Abdulkarim.Alansari@outlook.com>
2023-02-22 19:45:24 +0300

    Update 2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
  // Import the functions you need from the SDKs you need
  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
- import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
+ import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
+ import { getFirestore, collection, getDoc, getDocs, doc, query} from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js'
 
  // Your web app's Firebase configuration
  const firebaseConfig = {
@@ -15,7 +16,50 @@
  // Initialize Firebase
  const app = initializeApp(firebaseConfig);
  const auth = getAuth();
- console.log(app);
+ const db = getFirestore(app);
+
+
+// Get users data from firestore
+const userList = document.querySelector('.ta');
+
+
+getDocs(collection(db, "userData")).then(snapshot => {
+  var index = 0;
+  let html = '';
+
+  snapshot.forEach((doc) => {
+    const getUsers = doc.data();
+    const li = `
+    <tr>
+        <td>${index+=1}</td>
+        <td>${getUsers.fullname}</td>
+        <td>${getUsers.email}</td>
+        <td>${getUsers.department}</td>
+        <td>${getUsers.division}</td>
+        <td>${getUsers.role}</td>
+    <tr>
+      `;
+    html += li;
+  });
+  userList.innerHTML = html;
+
+});
+  
+
+
+
+
+
+// Check if user login
+auth.onAuthStateChanged(user => {
+  if (user) {
+    console.log('User logged in ' , user);
+  } else {
+    console.log('User logged out ' , user);
+
+  }
+});
+
 
 
 //  Login Function
@@ -43,6 +87,8 @@ window.logout = function(e){
     e.preventDefault();
     signOut(auth).then(() => {
         alert("Logout successfully");
+        console.log("Logout");
+
         window.location.replace('index.html');
       }).catch((error) => {
         alert("Error: " + error);
@@ -50,4 +96,25 @@ window.logout = function(e){
 }
 
 
+// ===================
+// add hovered class to selected list item
+let list = document.querySelectorAll(".navigation li");
 
+function activeLink() {
+  list.forEach((item) => {
+    item.classList.remove("hovered");
+  });
+  this.classList.add("hovered");
+}
+
+list.forEach((item) => item.addEventListener("mouseover", activeLink));
+
+// Menu Toggle
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+toggle.onclick = function () {
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+};
